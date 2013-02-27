490e510072dd01ea358ea20a9aa15589e1f81a1a
helene <helene@heleneeeepc-1025CE.(none)>
2013-02-27 22:36:53 +0100

    alloc et set

--- matrix.c
+++ matrix.c
@@ -43,43 +43,31 @@ struct matrix {
 
 matrix_t * matrix_alloc (int rows, int columns)
 {
-	// the structure
-	matrix_t *pMatrix = (matrix_t*) malloc (sizeof (matrix_t));
-	if (! pMatrix)
-	{
-		fprintf (stderr, "ERROR: when trying to allocate the memory for the matrix\n");
-		return NULL;
-	}
+  // the structure
+  matrix_t *pMatrix = (matrix_t*) malloc (sizeof (matrix_t));
+  if (! pMatrix)
+    {
+      fprintf (stderr, "ERROR: when trying to allocate the memory for the matrix\n");
+      return NULL;
+    }
+	
+  // set the numbers of rows and columns
+  pmatrix->iNbRows = rows;
+  pmatrix->inbCols = columns;
+	
+  // set the columns and rows to null, new matrix consdered as empty
+  pmatrix->pFirstRow = NULL;
+  pmatrix->pFirstCol = NULL;
+	
+  // set de state of the matrix
+  pMatrix->bState = MATRIX_BUSY; // busy par défaut
 
-	// tabs of tabs
-	pMatrix->iMatrix = (int **) malloc (rows * sizeof (int*));
-
-	int i, j;
-	for (i = 0 ; i < rows ; i++)
-	{
-		// tabs in tabs
-		pMatrix->iMatrix[i] = (int *) malloc (columns * sizeof (int));
-		if (! pMatrix->iMatrix[i]) // IF_ERROR
-		{
-			for (j = 0 ; j < i ; j++)
-				free (pMatrix->iMatrix[j]);
-
-			free (pMatrix->iMatrix);
-			free (pMatrix);
-			fprintf (stderr, "ERROR: when trying to allocate the memory for the row %d\n", i);
-			return NULL;
-		}
-	}
-
-	pMatrix->iRows = rows;
-	pMatrix->iCols = columns;
-	pMatrix->bState = MATRIX_BUSY; // busy par défaut
-
-	return pMatrix;
+  return pMatrix;
 }
 
 void matrix_free (matrix_t *m)
 {
+			
 	IF_ERROR (m, __func__);
 
 	for (int i = 0 ; i < m->iRows ; i++)
@@ -91,12 +79,85 @@ void matrix_free (matrix_t *m)
 
 void matrix_set (matrix_t *m, int i, int j, int val)
 {
+  if (i > m->iNbRows || j > m->iNbCols){
+    //erreur, on sort des bornes
+  }
+	
+  // cas ou pas encore de ligne implique pas encore de colone non plus
+  if (m->pFirstRow == NULL) {
+		
+    RowInfo *pNewRow = (RowInfo*) malloc (sizeof (RowInfo));
+    ColInfo *pNewCol = (ColInfo*) malloc (sizeof (ColInfo));
+    Node *pNewNode = (Node*) malloc (sizeof (Node));
+		
+    // test si les mallocs se sont bien passes
+    if (!pNewRow || !pNewCol || !pNewNode){
+      fprintf (stderr, "ERROR: when trying to allocate the memory for the new node\n");
+      if (pNewRow){free(pNewRow);}
+      if (pNewCol){free(pNewCol);}
+      if (pNewNode){free(pNewNode);}
+
+    }else{
+
+      m->pFirstRow = pNewRow;
+      m->pFirstCol = pNewCol;
+	  
+      pNewRow->iRowNo = i;
+      pNewRow->pFirstNode = pNewNode;
+      pNewRow->pNewRow = NULL;
+
+      pNewCol->iColNo = j;
+      pNewCol->pFirstNode = pNewNode;
+      pNewCol->pNewCol = NULL;
+			
+      pNewNode->iRow = i;
+      pNewNode->iCol = j;
+      pNewNode->iData = val;
+      pNewNode->pNextRight = NULL;
+      pNewNode->pNextDown = NULL;		
+
+    }
+
+  }else{
+    
+    RowInfo *tempRow;
+    ColInfo *tempCol;
+
+    if (m->pFirstRow->iRowNo > i){
+      
+    
+    
+  }
+		
+	
+	
+	
+    RowInfo rowpres = NULL;
+    RowInfo row = m->pFirstRow;
+    ColInfo colpres = NULL;
+    ColInfo col = m->pFirstCol;
+	
+    int iterpres = 0;
+    int iternew = row->iRowNo;
+    bool end = false;
+    while (iternew < i && iternew <= m->iNbRows && !end){
+      iterpres = iternew;
+      rowpres = row;
+      if (rowpres->pNextRow == NULL
+	  iternew = rowpres->iRowNo;
+	  row = rowpres->pNextRow;
+	  }
+      if (! iternew == i) { // row doesn't exist	
+		
+		 
+	
+	
 	IF_ERROR (m, __func__);
-		// TODO: error? What do we do? + check limit matrix?*/
-		// maybe useless to check each time... maybe safer but it's just used by us...
+	// TODO: error? What do we do? + check limit matrix?*/
+	// maybe useless to check each time... maybe safer but it's just used by us...
 
 	m->iMatrix[i][j] = val;
-}
+      }
 
 int matrix_get (matrix_t *m, int i, int j)
 {
--- matrix.h
+++ matrix.h
@@ -13,49 +13,41 @@ typedef struct matrix matrix_t;
 /**
  * allocate the right memory
  */
-matrix_t *
-matrix_alloc (int rows, int columns);
+matrix_t *matrix_alloc (int rows, int columns);
 
 /**
  * free the allocated memory
  */
-void
-matrix_free (matrix_t *m);
+void matrix_free (matrix_t *m);
 
 /**
  * Set the value val in m at [i][j]
  */
-void
-matrix_set (matrix_t *m, int i, int j, int val);
+void matrix_set (matrix_t *m, int i, int j, int val);
 
 /**
  * Get the value in m at [i][j]
  */
-int
-matrix_get (matrix_t *m, int i, int j);
+int matrix_get (matrix_t *m, int i, int j);
 
 /**
  * Change the state of the matrix
  */
-void
-matrix_set_state (matrix_t *m, MatrixState state);
+void matrix_set_state (matrix_t *m, MatrixState state);
 
 /**
  * Get the state of the matrix
  */
-MatrixState
-matrix_get_state (matrix_t *m);
+MatrixState matrix_get_state (matrix_t *m);
 
 /**
  * Print to stdout the matrix
  */
-void
-matrix_print (matrix_t *m);
+void matrix_print (matrix_t *m);
 
 /**
  * An easy way to do the matricial product between two matrix
  */
-matrix_t *
-matrix_product (matrix_t *m1, matrix_t *m2);
+matrix_t *matrix_product (matrix_t *m1, matrix_t *m2);
 
 #endif
