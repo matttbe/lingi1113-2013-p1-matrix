26d73b9cea3cc70af179090e2be5ab616e51ebad
Matthieu Baerts <matttbe@gmail.com>
2013-03-06 03:45:23 +0100

    Removed old

--- old/consumer.h
+++
@@ -1,6 +0,0 @@
-#ifndef __CONSUMER_H__
-#define __CONSUMER_H__
-
-void *consumer (void *arg);
-
-#endif /* __CONSUMER_H__ */
--- old/producer.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PRODUCER_H__
-#define __PRODUCER_H__
-
-void* producer (void*);
-
-
-
-
-#endif /* __PRODUCER_H__ */
